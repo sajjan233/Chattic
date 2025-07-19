cb051219393570ccec48464940cfc7c739684c41
sajjan_ubantu <sajjan@ubantu.com>
2025-07-19 19:52:18 +0530

    set clien in server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
 const app = express()
+import path from 'path';
+let dir = path.dirname('')
 
-
+app.use('public', express.static(dir + '/public'));
+app.use(express.static(path.join(dir, './client')));
 
 
 app.listen(5000,() => {
