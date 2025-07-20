b35180bc345de3091f2c00f02616d6d5464c1537
EC2 Default User <ec2-user@ip-172-31-15-220.ap-south-1.compute.internal>
2025-07-20 03:47:01 +0000

    production changes

--- server/index.js
+++ server/index.js
@@ -4,11 +4,31 @@ import { } from 'dotenv/config'
 import path from 'path';
 import router from './src/routes/index.js';
 let dir = path.dirname('')
+
+
+import { fileURLToPath } from 'url';
+
+// Required to use __dirname with ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// ✅ Load .env from server folder
+dotenv.config({
+  path: path.resolve(__dirname, '.env')
+});
+
+
+
+
+
+
+
+
 const app = express()
 app.use(express.json())
 const { MONGODB_PROD, MONGODB_STAGING, PORT, NODE_ENV } = process.env
 let mongodbUrl = NODE_ENV == 'Production' ? MONGODB_PROD : MONGODB_STAGING;
-
+console.log(process.env);
 
 mongoose.connect(mongodbUrl, {
 
