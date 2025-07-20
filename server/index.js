18c17e16de071c17d3937a766144cff518f70d15
sajjan <sajjankumarhsr23@gmail.com>
2025-07-20 06:29:28 +0530

    mongodb connect

--- server/index.js
+++ server/index.js
@@ -1,5 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import {} from 'dotenv/config'
 const app = express()
+const {MONGODB} = process.env
+console.log(MONGODB);
+
+mongoose.connect('mongodb+srv://sajjankumarhsr23:sajjankumarhsr23@personal.nlb6rsv.mongodb.net/?retryWrites=true&w=majority&appName=personal').then((E) => {
+    console.log("connect mongodb");
+}).catch((err) => {
+    console.log("mongodb_connection_err",err);
+    
+})
 
 
 
