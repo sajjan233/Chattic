11aeab5be17d78c244d3510330b265f652b1ba5e
sajjan <sajjankumarhsr23@gmail.com>
2025-07-20 08:07:09 +0530

    role setup

--- server/index.js
+++ server/index.js
@@ -1,25 +1,28 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import {} from 'dotenv/config'
+import { } from 'dotenv/config'
 import path from 'path';
+import router from './src/routes/index.js';
 let dir = path.dirname('')
 const app = express()
-const {MONGODB} = process.env
+app.use(express.json())
+const { MONGODB_PROD, MONGODB_STAGING, PORT, NODE_ENV } = process.env
+let mongodbUrl = NODE_ENV == 'Production' ? MONGODB_PROD : MONGODB_STAGING;
 
 
-mongoose.connect(MONGODB,{
-    
+mongoose.connect(mongodbUrl, {
+
 }).then(() => {
     console.log("connect mongodb");
 }).catch((err) => {
-    console.log("mongodb_connection_err",err);
-    
+    console.log("mongodb_connection_err", err);
+
 })
 
+app.use('/', router)
 app.use('public', express.static(dir + '/public'));
 app.use(express.static(path.join(dir, './client/build')));
 
-
-app.listen(5000,() => {
-    console.log("server run");  
+app.listen(PORT, () => {
+    console.log("server run");
 })
