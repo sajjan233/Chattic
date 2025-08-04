ac54f0393c71d6f6e31a95cdec63ba66a4d9f156
sajjan <sajjankumarhsr23@gmail.com>
2025-08-04 20:56:02 +0530

    some chnage

--- server/src/config/passport.js
+++ server/src/config/passport.js
@@ -4,6 +4,12 @@ import User from '../features/user/user.js'; // update path if needed
 import dotenv from 'dotenv';
 dotenv.config();
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
 const JwtStrategy = passportJwt.Strategy;
 const ExtractJwt = passportJwt.ExtractJwt;
 console.log("JWT_SECRET:", process.env.JWT_SECRET)
